2aba887691bf49357cbb154c410f98b65b2b69d0
WyaletGarden <wyalergarden@gmail.com>
2025-06-02 00:04:11 +0700

    fix

--- src/lib/config.js
+++ src/lib/config.js
@@ -14,6 +14,15 @@ export const siteAuthor = '- find and change this text in src/lib/config.js'
 // Controls how many posts are shown per page on the main blog index pages
 export const postsPerPage = 10
 
+// svelte.config.js
+export default {
+  kit: {
+    prerender: {
+      handleHttpError: 'ignore' // Hoặc 'ignore' để bỏ qua lỗi
+    }
+  }
+};
+
 // Edit this to alter the main nav menu. (Also used by the footer and mobile nav.)
 export const navItems = [
 	{
